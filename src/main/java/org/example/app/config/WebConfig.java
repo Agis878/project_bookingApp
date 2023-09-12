d6313800617bb0715280833118d98781627dd500
Agis878 <a.lukasik@op.pl>
2023-09-12 12:47:12 +0200

    Model

---
+++ src/main/java/org/example/app/config/WebConfig.java
@@ -0,0 +1,28 @@
+package pl.coderslab.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import pl.coderslab.app.author.AuthorConverter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        WebMvcConfigurer.super.configureViewResolvers(registry);
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(authorConverter());
+    }
+
+    @Bean
+    public AuthorConverter authorConverter() {
+        return new AuthorConverter();
+    }
+}
---
+++ src/main/java/org/example/app/user/userDao.java
@@ -0,0 +1,4 @@
+package org.example.app.user;
+
+public class userDao {
+}
